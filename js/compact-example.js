2f8504c8b3dfcac624867043638b547a37de53d0
Alexey Kulikov <a.kulikov@gmail.com>
2012-03-09 10:27:10 +0100

    Updated the Example to be used with a Replica Set (any member)

--- js/compact-example.js
+++ js/compact-example.js
@@ -1,38 +1,63 @@
-// assumption is we are primary
+print("---------------------------------");
+print(" MongoDB Maintenance Script V0.1 ");
+print("    For use in a Replica Set     ");
+print("     Author: Alexey Kulikov      ");
+print("---------------------------------");
+print(" ")
 
-assert( rs.isMaster().setName, "not a repl set" );  // assuming this is a repl set
-assert( rs.isMaster().ismaster, "we are not primary and we were assuming that");
-
-// we want to compact, but in the background
-
-// so step down...
-try {
-    rs.stepDown();
-} catch(e) { 
-    print("exception:" + e);
-}
-
-// after stepdown connections are dropped. do an operation to cause reconnect:
-rs.isMaster();
-
-// now ready to go.  
-
-// wait for another node to become primary -- it may need data from us for the last 
-// small sliver of time, and if we are already compacting it cannot get it while the 
-// compaction is running.
-
-while( 1 ) { 
-    var m = rs.isMaster();
-    if( m.ismaster ) {
-        print("ERROR: no one took over during our stepDown duration. we are primary again!");
-	assert(false);
+// assuming we are a replica set ;-)
+if(rs.isMaster().setName){
+    try {
+        //check if the script is run against a master
+        if(rs.isMaster().ismaster){ //if so, step down as master        
+            print("Connected to a PRIMARY node")
+            print("Will try to step down as primary");
+            rs.stepDown();
+        
+            // after stepdown connections are dropped. do an operation to cause reconnect:
+            rs.isMaster();            
+            
+            // now ready to go.      
+            // wait for another node to become primary -- it may need data from us for the last 
+            // small sliver of time, and if we are already compacting it cannot get it while the 
+            // compaction is running.            
+            var counter = 1;
+            while( 1 ) { 
+                var m = rs.isMaster();
+                if( m.ismaster ) {
+                    print("ERROR: no one took over during our stepDown duration. we are primary again!");
+                    assert(false);
+                }
+                
+                if( m.primary ){ // someone else is, great
+                    print("new master host is: "+m.primary);
+                    break; 
+                }
+                print("waiting "+counter+" seconds");
+                sleep(1000);
+                counter++;
+            }
+        }else{
+            print("Connected to a SECONDARY node");
+            print("Going into Recovery Mode and Compacting");            
+        }
+        
+        // someone else is primary, so we are ready to proceed with a compaction
+        print(" ");
+        print("Compacting...");
+        print("- wengine_queue");
+        printjson( db.runCommand({compact:"wengine_queue",dev:true}) );
+        print("- wengine_log");
+        printjson( db.runCommand({compact:"wengine_log",dev:true}) );
+        print("- wengine_workers");
+        printjson( db.runCommand({compact:"wengine_workers",dev:true}) );
+        print("- wengine_workers_log");
+        printjson( db.runCommand({compact:"wengine_workers_log",dev:true}) );
+        print(" ");
+    
+    } catch(e) { 
+        print(" ");
+        print("ACHTUNG! Exception:" + e);
+        print(" ");
     }
-    if( m.primary )
-        break; // someone else is, great
-    print("waiting");
-    sleep(1000);
 }
-
-// someone else is primary, so we are ready to proceed with a compaction
-print("Compacting...");
-printjson( db.runCommand({compact:"mycollection",dev:true}) );
